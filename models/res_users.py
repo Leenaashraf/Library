f7bf5300ed13cfd7c4026d9f9451a1933a7d5b15
Leena <157684646+Leenaashraf@users.noreply.github.com>
2026-07-03 01:45:11 +0400

    Add wishlist_book_ids to ResUsers model

--- models/res_users.py
+++ models/res_users.py
@@ -4,4 +4,4 @@ class ResUsers(models.Model):
     _inherit = "res.users"
 
     book_ids = fields.One2many("library.book", "librarian_id", domain=[('state', 'in', ('available', 'not_available'))])
-    
+    wishlist_book_ids = fields.Many2many("library.book", "library_book_wishlist_rel", "user_id", "book_id", string="My Wishlist")
