92e6ca6744e30dc9ce41b68571cc2b5286d37e26
Leena <157684646+Leenaashraf@users.noreply.github.com>
2026-07-02 17:57:51 +0400

    Update book_ids domain to include 'not_available'

--- models/res_users.py
+++ models/res_users.py
@@ -3,4 +3,4 @@ from odoo import fields, models
 class ResUsers(models.Model):
     _inherit = "res.users"
 
-    book_ids = fields.One2many("library.book", "librarian_id", domain=[('state', 'in', ('available', 'borrowed'))])
+    book_ids = fields.One2many("library.book", "librarian_id", domain=[('state', 'in', ('available', 'not_available'))])
