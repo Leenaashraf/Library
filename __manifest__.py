a7399bf9ad6ddae7950017237296a24f1244fac3
Aabid Anas <aabidanas6@gmail.com>
2026-07-02 16:39:53 +0400

    Rename module and add data files
    Updated the name and added data files for library management.

--- __manifest__.py
+++ __manifest__.py
@@ -1,10 +1,17 @@
 {
-    'name': 'Library Management',
+    'name': 'Library Tracker',
     'version': '1.0',
-    'summary': 'Manage books and library operations',
-    'category': 'Services',
     'depends': ['base'],
-    'data': [],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/library_book_views.xml',
+        'views/library_member_views.xml',
+        'views/library_borrow_record_views.xml',
+        'views/library_category_views.xml',
+        'views/library_book_tag_views.xml',
+        'views/res_users_views.xml',
+        'views/library_menus.xml',
+    ],
     'installable': True,
     'application': True,
 }
