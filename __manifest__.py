7050dd4ae5324ff566e063545a2834cd878716c6
Leena Ashraf <leenaash7@gmail.com>
2026-07-02 16:07:43 +0400

    Initial skeleton for Odoo Library module

---
+++ __manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'Library Management',
+    'version': '1.0',
+    'summary': 'Manage books and library operations',
+    'category': 'Services',
+    'depends': ['base'],
+    'data': [],
+    'installable': True,
+    'application': True,
+}
